d0d554edc212d8a6a92f5c889fb658012f69f638
pyjshtml <62919016+pyjshtml@users.noreply.github.com>
2020-06-05 13:02:02 -0400

    --

---
+++ Exercise01_01.java
@@ -0,0 +1,6 @@
+public class Exercise01_01{
+    public static void main(String[] args){
+        String[] out = new String[]{"Welcome to Java", "Welcome to Computer Science", "Programming is Fun"};
+        System.out.println(String.join("\n",out));
+    }
+}
---
+++ Exercise01_05.java
@@ -0,0 +1,7 @@
+public class Exercise01_05{
+  public static void main(String[] args){
+    double numerator = 9.5 * 4.5 - 2.5 * 3;
+    double denominator = 45.5 - 3.5;
+    System.out.println(numerator / denominator);
+  }
+}
