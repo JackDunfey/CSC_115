2e5f64b8599335ea700122f828a1414cff8d1551
pyjshtml <62919016+pyjshtml@users.noreply.github.com>
2022-02-07 14:13:27 -0500

    Compressed Exercise05_47
    Old Code:
    public class Exercise05_47{
      static java.util.Scanner stdin = new java.util.Scanner(System.in);
      public static void main(String[] args) {
        System.out.print("Enter the first 12 digits of an ISBN-13 as a string: ");
        String s = stdin.next();
        try{
          int total = 0;
          for(int i = 0; i < 12; i++){
            total += ((int) s.charAt(i) - 48) * (i%2==0 ? 1 : 3);
          }
          System.out.println("The ISBN-13 number is "+s+(10-total%10 == 10 ? 0 : 10-total%10));
        } catch(Exception e){
          System.out.println(s+" is an invalid input");
        }
      }
    }

--- Exercise05_47.java
+++ Exercise05_47.java
@@ -1,14 +1,12 @@
 public class Exercise05_47{
-  static java.util.Scanner stdin = new java.util.Scanner(System.in);
   public static void main(String[] args) {
+    var stdin = new java.util.Scanner(System.in);
     System.out.print("Enter the first 12 digits of an ISBN-13 as a string: ");
     String s = stdin.next();
     try{
       int total = 0;
-      for(int i = 0; i < 12; i++){
-        total += ((int) s.charAt(i) - 48) * (i%2==0 ? 1 : 3);
-      }
-      System.out.println("The ISBN-13 number is "+s+(10-total%10 == 10 ? 0 : 10-total%10));
+      for(int i = 0; i < 12; total += ((int) s.charAt(i) - 48) * (i++%2==0 ? 1 : 3));
+      System.out.println("The ISBN-13 number is "+s+(total%10==0?0:10-total%10));
     } catch(Exception e){
       System.out.println(s+" is an invalid input");
     }
