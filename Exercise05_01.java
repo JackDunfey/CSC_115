f1d4f00305ba8c0abb810b2c2c59ce15f67466b4
pyjshtml <62919016+pyjshtml@users.noreply.github.com>
2022-02-07 14:01:06 -0500

    Compressed Exercise05_01
    Old Code:
    public class Exercise05_01{
      static java.util.Scanner stdin = new java.util.Scanner(System.in);
      public static void main(String[] args){
        java.util.ArrayList<Integer> array = new java.util.ArrayList<>();
        for(;;){
          int n = stdin.nextInt();
          if(n == 0){
            break;
          }
          array.add(n);
        }
        if(array.isEmpty()){
          System.out.println("No numbers are entered except 0");
        } else {
          int positive_count = 0;
          int negative_count = 0;
          int total = 0;
          for(int i = 0; i < array.size(); i++){
            if(array.get(i) > 0){
              positive_count++;
            } else {
              negative_count++;
            }
            total += array.get(i);
          }
          System.out.println("The number of positives is "+positive_count);
          System.out.println("The number of negatives is "+negative_count);
          System.out.println("The total is "+total);
          System.out.println("The average is "+((double) total / array.size()));
        }
      }
    }

--- Exercise05_01.java
+++ Exercise05_01.java
@@ -1,30 +1,22 @@
-public class Exercise05_01{
-  static java.util.Scanner stdin = new java.util.Scanner(System.in);
+import java.util.*;
+public class Exercose05_01{
   public static void main(String[] args){
-    java.util.ArrayList<Integer> array = new java.util.ArrayList<>();
+    var stdin = new Scanner(System.in);
+    var array = new ArrayList<Integer>();
     for(;;){
       int n = stdin.nextInt();
-      if(n == 0){
+      if(n == 0)
         break;
-      }
       array.add(n);
     }
     if(array.isEmpty()){
       System.out.println("No numbers are entered except 0");
     } else {
-      int positive_count = 0;
-      int negative_count = 0;
+      int[] count = new int[2];
       int total = 0;
-      for(int i = 0; i < array.size(); i++){
-        if(array.get(i) > 0){
-          positive_count++;
-        } else {
-          negative_count++;
-        }
-        total += array.get(i);
-      }
-      System.out.println("The number of positives is "+positive_count);
-      System.out.println("The number of negatives is "+negative_count);
+      for(int i = 0; i < array.size(); count[array.get(i)>0?0:1]++, total+=array.get(i++));
+      System.out.println("The number of positives is "+count[0]);
+      System.out.println("The number of negatives is "+count[1]);
       System.out.println("The total is "+total);
       System.out.println("The average is "+((double) total / array.size()));
     }
